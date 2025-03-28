7c7dc92ac6fe4d131949743f686858e610aaa3cc
Muriellemsc <muriellemariesimone@gmail.com>
2025-03-28 15:24:28 +0000

    project structure done and database setting up

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"github.com/Azertdev/FiberTest/config"
+	"github.com/Azertdev/FiberTest/internal/handlers"
+	"github.com/Azertdev/FiberTest/internal/repositories"
+	"github.com/Azertdev/FiberTest/internal/routes"
+	"github.com/Azertdev/FiberTest/internal/services"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	config.InitDB()
+
+	userRepo := repositories.NewUserRepository(config.DB)
+
+	// Initialize services
+	allServices := services.NewAllServices(userRepo)
+	
+	userHandler := handlers.NewUserHandler(allServices.UserService)
+
 	app := fiber.New()
+	routes.SetupUserRoutes(app, userHandler)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Listen(":3001")
+	log.Fatal(app.Listen(":3001"))
 }
---
+++ config/database.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/Azertdev/FiberTest/internal/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	// Charger les variables d’environnement
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erreur lors du chargement du fichier .env")
+	}
+
+	dsn := os.Getenv("DB_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Erreur de connexion à la base de données :", err)
+	}
+
+	DB = db
+	fmt.Println("✅ Connecté à PostgreSQL")
+
+	// Auto-migration des modèles
+	db.AutoMigrate(&models.User{})
+}
